b410577ce09eac30a32b1256f0f45871eeb1d01b
Moksh Keloo <moksh@integratedwebworks.com>
2025-01-09 14:45:13 -0500

    Add initial setup for Tailwind CSS and remove unused components

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center h-full bg-blue-950">
+      <Text className="font-bold text-2xl text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
